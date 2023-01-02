f73f75b6e8abe65bc33ad53a5ecf161f6d416973
danielcastilla <danielcastilla@gmail.com>
2023-01-02 17:36:59 +0100

    Added data.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import PostElementForm from './components/PostElementForm'
-import ListElements from './components/ListElements'
 
 function App() {
   return (
--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
 
-app.get('/express_backend', (req, res) => {
+
+app.get('/', (req, res) => {
     res.send({ express: 'YOU EXPRESS BACKEND IS CONNECT TO REACT'})
 })
