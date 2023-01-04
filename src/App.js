f7ad845eb4ec8f58245022869a4fe838d83fa050
danielcastilla <danielcastilla@gmail.com>
2023-01-04 19:30:12 +0100

    Updated app.js and index.html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import PostElementForm from './components/PostElementForm'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <PostElementForm/>
       </header>
     </div>
   );
