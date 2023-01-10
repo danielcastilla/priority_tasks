f65fc91408245986fc0861f44781c1d157eb7a7e
danielcastilla <danielcastilla@gmail.com>
2023-01-10 08:00:14 +0100

    Added servcies and commons

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import './App.css';
+import React, { Component } from "react";
+//import ListElements from './components/ListElements.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div>
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+          <a href="/tasks" className="navbar-brand">
+            bezKoder
+          </a>
+          <div className="navbar-nav mr-auto">
+            <li className="nav-item">
+              <Link to={"/tasks"} className="nav-link">
+                Tutorials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/add"} className="nav-link">
+                Add
+              </Link>
+            </li>
+          </div>
+        </nav>
+
+        <div className="container mt-3">
+          <Routes>
+            <Route path="/" element={<TasksList/>} />
+            <Route path="/tasks" element={<TasksList/>} />
+            <Route path="/add" element={<AddTask/>} />
+            <Route path="/tasks/:id" element={<Task/>} />
+          </Routes>
+        </div>
+      </div>
+
+    );
+  }
 }
+
 
 export default App;
--- src/components/ListElements.js
+++ src/components/ListElements.js
@@ -1,26 +1,23 @@
-import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';  
+import React, { useEffect } from 'react';
+import {Container ,Card, Col, Button} from 'react-bootstrap'; 
+//import useFetch from "./useFetch"; 
 
-function ListElements(props) {
-    return (
-        <div>
-            <div class="elemento text-limited">
-          <div class="linea1">
-            <div class="seguir">Sigue este tema</div>
-            <div class="creado">Creado el 23 de diciembre</div>
-            <div class="actualizado">Actualizado hace 1 hora</div>
-          </div>
-          <div class="titular text-limited">Titular de la tarea</div>
-          <div class="lineaTags">
-            <div class="tag">tag1</div>
-            <div class="tag">tag2</div>
-            <div class="tag">tag3</div>
-          </div>
-          <div class="descripcion">Lorem ipsum dolor sit amet consectetur adipisicing elit. A asperiores recusandae, repellat excepturi commodi enim ducimus libero labore officia dicta soluta laborum velit quasi distinctio doloribus ipsum obcaecati omnis unde rem minus necessitatibus minima officiis! Officiis, architecto. Explicabo blanditiis ut voluptate iste nemo omnis voluptas quo, tempore velit voluptatem doloribus. In optio ex, cumque tempore, totam, sunt iusto ipsam ab magni accusantium quae atque voluptas hic incidunt vero dolore illo earum autem. Harum soluta illum labore repellendus placeat, aliquam reiciendis error nam ullam deserunt architecto aliquid quidem quaerat dolorem quibusdam autem suscipit et minus quod optio est, nulla provident? Incidunt.</div>
-        </div>
-        <div class="elemento">Elemento 2</div>
-        <div class="elemento">Elemento 3</div>
-        </div>
-    );
+function ListElements() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Card.Link href="#">Card Link</Card.Link>
+        <Card.Link href="#">Another Link</Card.Link>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default ListElements;
